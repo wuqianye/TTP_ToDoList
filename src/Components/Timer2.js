f3d6c57a795b11a2f68aeece1a14f7847a8181a5
Gisellen <giselle.domingo.x@gmail.com>
2021-06-05 14:18:55 -0400

    Fixed time bug and added increase/decrease buttons

--- src/Components/Timer2.js
+++ src/Components/Timer2.js
@@ -5,41 +5,74 @@ export default class Timer2 extends Component {
   state = {
     minutes: 1,
     seconds: 5,
+    onStart: false,
+    isRunning: false,
   };
 
-  componentDidMount(){
+  startCountdown = () => {
+    this.setState({ onStart: true });
+    this.timestart();
+  };
+
+  timestart = () => {
+    //starts the countdown timer
+    if(this.state.isRunning === false){
     this.myInterval = setInterval(() => {
-      const {seconds, minutes} = this.state;
-      if (seconds > 0){
-        this.setState (({seconds}) =>({
-          seconds: seconds-1
-        }))
-      } else if (seconds === 0){
-        if (minutes === 0){
-          clearInterval(this.myInterval)
+      const { seconds, minutes, isRunning} = this.state;
+      this.setState({isRunning: true});
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1,
+        }));
+      } else if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval);
         } else {
-          this.setState(({minutes}) => ({
-            minutes: minutes - 1, 
-            seconds: 59
-          }))
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59,
+          }));
         }
       }
-    }, 1000)
-  }
+    }, 1000);
+  } else return;
+  };
 
+  timepause = () => {
+    //pauses the countdown timer
+    this.setState({isRunning: false})
+    clearInterval(this.myInterval);
+  };
+
+  increaseMin = () => {
+    
+  }
   render() {
-    const { minutes, seconds } = this.state;
+    const { minutes, seconds, onStart } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
+        {/* timer */}
+        <div class="time-container">
         <div style={{ fontSize: "100px" }}>
           <span>{minutes < 10 ? `0${minutes}` : minutes}</span>:
           <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
         </div>
-        <p></p>
-        <button class="time-button">Start</button>
-        <button class="time-button">Pause</button>
-        <button class="time-button">Resume</button>
-        <button class="time-button">Restart</button>
+        {/* arrows */}
+        <div class="timer-buttons">
+          <button>+</button>
+          <button>-</button>
+        </div>
+        </div>
+        {/* buttons */}
+        {!onStart ? (
+          <button class="start-button" onClick={this.startCountdown}>START TIMER</button>
+        ) : (
+          <div>
+            <button class="time-button" onClick={this.timepause}>Pause</button>
+            <button class="time-button" onClick={this.timestart}>Resume</button>
+            <button class="time-button">Restart</button>
+          </div>
+        )}
       </div>
     );
   }
