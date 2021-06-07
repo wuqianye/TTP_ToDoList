8bb35a3bbb61271ec20ddc254d7a95dadf137a63
Gisellen <giselle.domingo.x@gmail.com>
2021-06-07 00:04:48 -0400

    added a functioning Restart button

--- src/Components/Timer2.js
+++ src/Components/Timer2.js
@@ -4,33 +4,38 @@ import { render } from "react-dom";
 export default class Timer2 extends Component {
   state = {
     minutes: 1,
-    seconds: 5,
+    seconds: 0,
+    currentMinutes: 0,
+    currentSeconds: 0,
     onStart: false,
     isRunning: false,
   };
 
-  startCountdown = () => {
+
+
+  startCountdown = () => { //starts the countdown officially, showing the buttons to modify
     this.setState({ onStart: true });
     this.timestart();
+    this.setState(({currentMinutes: this.state.minutes})) // set the current minutes to the time it is now.
+    
   };
 
-  timestart = () => {
-    //starts the countdown timer
+  timestart = () => {  //starts the countdown timer
     if(this.state.isRunning === false){
     this.myInterval = setInterval(() => {
-      const { seconds, minutes, isRunning} = this.state;
+      const { currentSeconds, currentMinutes, isRunning} = this.state;
       this.setState({isRunning: true});
-      if (seconds > 0) {
-        this.setState(({ seconds }) => ({
-          seconds: seconds - 1,
+      if (currentSeconds > 0) {
+        this.setState(({ currentSeconds }) => ({
+          currentSeconds: currentSeconds - 1,
         }));
-      } else if (seconds === 0) {
-        if (minutes === 0) {
+      } else if (currentSeconds === 0) {
+        if (currentMinutes === 0) {
           clearInterval(this.myInterval);
         } else {
-          this.setState(({ minutes }) => ({
-            minutes: minutes - 1,
-            seconds: 59,
+          this.setState(({ currentMinutes }) => ({
+            currentMinutes: currentMinutes - 1,
+            currentSeconds: 59,
           }));
         }
       }
@@ -38,24 +43,33 @@ export default class Timer2 extends Component {
   } else return;
   };
 
-  timepause = () => {
-    //pauses the countdown timer
+  timepause = () => {    //pauses the countdown timer
     this.setState({isRunning: false})
     clearInterval(this.myInterval);
   };
 
-  increaseMin = () => {
+  timeRestart = () => { //restarts the time to what the user originally in
+    this.timepause();
+    const { currentSeconds, currentMinutes} = this.state;
+    this.setState(({
+      onStart:false,
+      currentSeconds: this.state.seconds,
+      currentMinutes: this.state.minutes
+    }));
+  }
+
+  increaseMin = () => { //WIP
     
   }
   render() {
-    const { minutes, seconds, onStart } = this.state;
+    const { currentMinutes, currentSeconds, onStart } = this.state;
     return (
       <div style={{ textAlign: "center" }}>
         {/* timer */}
         <div class="time-container">
         <div style={{ fontSize: "100px" }}>
-          <span>{minutes < 10 ? `0${minutes}` : minutes}</span>:
-          <span>{seconds < 10 ? `0${seconds}` : seconds}</span>
+          <span>{currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes}</span>:
+          <span>{currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds}</span>
         </div>
         {/* arrows */}
         <div class="timer-buttons">
@@ -70,7 +84,7 @@ export default class Timer2 extends Component {
           <div>
             <button class="time-button" onClick={this.timepause}>Pause</button>
             <button class="time-button" onClick={this.timestart}>Resume</button>
-            <button class="time-button">Restart</button>
+            <button class="time-button" onClick={this.timeRestart}>Restart</button>
           </div>
         )}
       </div>
